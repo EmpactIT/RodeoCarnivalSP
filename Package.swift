362e2f4472b532f6a75031eca2c3bcdc5b68b0cb
Bernardo Iribarne <bernardo.iribarne@empactit.com>
2024-10-31 13:38:52 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
                 .target(name: "webview_flutter_wkwebview"),
                 .target(name: "launch_review"),
                 .target(name: "sqflite"),
-                .target(name: "Reachability"),
+                //.target(name: "Reachability"),
                 .target(name: "store_redirect"),
                 .target(name: "connectivity_plus"),
                 .target(name: "pay_ios"),
@@ -102,10 +102,10 @@ let package = Package(
                     name: "sqflite",
                     path: "./Sources/binaryFrameworks/Release/sqflite.xcframework"
                 ),
-        .binaryTarget(
+        /*.binaryTarget(
                     name: "Reachability",
                     path: "./Sources/binaryFrameworks/Release/Reachability.xcframework"
-                ),
+                ),*/
         .binaryTarget(
                     name: "store_redirect",
                     path: "./Sources/binaryFrameworks/Release/store_redirect.xcframework"
